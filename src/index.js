05996a76c8ea2a732ddbec06de979478eb05d2fe
hirashoaib <hira@panacloud.com>
2019-06-16 15:31:22 +0500

    implemented store on index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers'
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
+function configureStore() {
+    return createStore(reducer, {}, applyMiddleware(thunk, logger));
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={configureStore()}>
+                    <App />
+                </Provider>, document.getElementById('root'));
 
